d583921347c5040cab4dbc676057b962c9c0c950
Bella P. <bellaperezppg@gmail.com>
2025-03-25 17:58:01 -0400

    Beginnings of the Main Menu

---
+++ SkellyCreatureBuilder/Assets/MainMenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public string loadLvl;
+    public void ChangeScene()
+    {
+
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
