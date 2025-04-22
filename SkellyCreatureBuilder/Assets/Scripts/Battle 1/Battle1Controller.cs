d0d07be2ff2ed3bcde9740c0eafcd56cec2eaf6c
Bella P. <bellaperezppg@gmail.com>
2025-04-22 16:19:24 -0400

    Fixed randomize scene and added
    Added Scenes to build settings

--- SkellyCreatureBuilder/Assets/Scripts/Battle 1/Battle1Controller.cs
+++ SkellyCreatureBuilder/Assets/Scripts/Battle 1/Battle1Controller.cs
@@ -5,23 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Battle1Controller : MonoBehaviour
 {
-    //[SerializeField]
-    //private string[] sceneNames = {
-    //    "Battle2",
-    //    "Battle3",
-    //    "Scene3",
-    //    "Scene4"
-    //};
+    [SerializeField]
+    private string[] sceneNames =
+        {
+        "Battle2",
+        "Battle3",
+        "TriviaMemory",
+        "TrueFalseMemory"
+        };
+
+    public void LoadRandomScene()
+    {
+        int randomIndex = Random.Range(0, sceneNames.Length);
+        SceneManager.LoadScene(sceneNames[randomIndex]);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        //int randomIndex = Random.Range(0, sceneNames.Length);
-       // SceneManager.LoadScene(sceneNames[randomIndex]);
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
